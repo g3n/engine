de86c6eb95bb8e871ad679913c1904a611a9270d
leonsal <leonel.salvadori@gmail.com>
2017-03-11 12:13:00 -0300

    doc update

--- gui/doc.go
+++ gui/doc.go
@@ -2,6 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package gui ...
+// Package gui implements the GUI infraestructure and several widgets.
 package gui
-
