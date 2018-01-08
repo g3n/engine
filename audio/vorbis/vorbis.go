21106982f591495ac863386f4f449512ffe0bcc5
leonsal <leonel.salvadori@gmail.com>
2018-01-08 13:34:14 -0200

    windows audio dlls...

--- audio/vorbis/vorbis.go
+++ audio/vorbis/vorbis.go
@@ -8,10 +8,10 @@ package vorbis
 
 // #cgo darwin   CFLAGS:  -DGO_DARWIN  -I/usr/include/vorbis
 // #cgo linux    CFLAGS:  -DGO_LINUX   -I/usr/include/vorbis
-// #cgo windows  CFLAGS:  -DGO_WINDOWS -I${SRCDIR}/../windows/oggvorbis/libvorbis/include/vorbis -I${SRCDIR}/../windows/oggvorbis/libogg/include
+// #cgo windows  CFLAGS:  -DGO_WINDOWS -I${SRCDIR}/../windows/libvorbis-1.3.5/include/vorbis -I${SRCDIR}/../windows/libogg-1.3.3/include
 // #cgo darwin   LDFLAGS: -lvorbis
 // #cgo linux    LDFLAGS: -lvorbis
-// #cgo windows  LDFLAGS: -lvorbis
+// #cgo windows  LDFLAGS: -L${SRCDIR}/../windows/bin -llibvorbis
 // #include "codec.h"
 import "C"
 
