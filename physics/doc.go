731f74045baea2395db99773b15652066fa27749
danaugrs <danaugrs@gmail.com>
2018-05-16 21:01:56 -0700

    started working on physics

---
+++ physics/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2016 The G3N Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package physics implements a basic physics engine.
+package physics
+
---
+++ physics/rigidbody.go
@@ -0,0 +1,17 @@
+// Copyright 2016 The G3N Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package physics
+
+// RigidBody represents a physics-driven solid body.
+type RigidBody struct {
+	// TODO :)
+}
+
+// NewRigidBody creates and returns a pointer to a new RigidBody.
+func NewRigidBody() *RigidBody {
+
+	b := new(RigidBody)
+	return b
+}
