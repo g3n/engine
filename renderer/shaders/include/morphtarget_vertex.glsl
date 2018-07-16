cec4fb98d6f49c04259f037c7b73accee96d02bc
Daniel Salvadori <danaugrs@gmail.com>
2018-07-16 18:50:30 -0300

    cleanup/docs

--- renderer/shaders/include/morphtarget_vertex.glsl
+++ renderer/shaders/include/morphtarget_vertex.glsl
@@ -1,6 +1,5 @@
 #ifdef MORPHTARGETS
 	vPosition += (MorphPosition{i} - VertexPosition) * morphTargetInfluences[{i}];
-//    vPosition = MorphPosition1;
   #ifdef MORPHTARGETS_NORMAL
 	vNormal += (MorphNormal{i} - VertexNormal) * morphTargetInfluences[{i}];
   #endif
