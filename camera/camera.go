7f80596f18d910fbb244cdba3ce4147c2b7f55a0
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-07-22 23:01:29 +0000

    Added SetAspect to the ICamera interface
    Merge https://github.com/g3n/engine/pull/57 first if you accept this change.

--- camera/camera.go
+++ camera/camera.go
@@ -16,6 +16,7 @@ type ICamera interface {
 	ViewMatrix(*math32.Matrix4)
 	ProjMatrix(*math32.Matrix4)
 	Project(*math32.Vector3) (*math32.Vector3, error)
+	SetAspect(float32)
 	Unproject(*math32.Vector3) (*math32.Vector3, error)
 	SetRaycaster(rc *core.Raycaster, x, y float32) error
 }
