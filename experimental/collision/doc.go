3c634c2b95966a1d2cd59379ca2c99e5aaa51d14
Daniel Salvadori <danaugrs@gmail.com>
2018-07-29 12:48:17 -0300

    fixed docs

--- experimental/collision/doc.go
+++ experimental/collision/doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package physics implements a basic physics engine.
+// Package collision implements collision related algorithms and data structures.
 // WARNING: This package is experimental and incomplete!
-package physics
+package collision
