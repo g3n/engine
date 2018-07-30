79fade7c3dc1058cd9eb80c7b9732c99e462d128
Daniel Salvadori <danaugrs@gmail.com>
2018-07-30 09:09:32 -0300

    fix gen.go docs

--- gui/assets/gen.go
+++ gui/assets/gen.go
@@ -2,7 +2,7 @@ package assets
 
 // To generate file with fonts binary data install "go-bindata" from:
 // https://github.com/go-bindata/go-bindata
-// >go get -u github.com/jteeuwen/go-bindata/...
+// > go get -u github.com/go-bindata/go-bindata/...
 
 //go:generate go-bindata -o data.go -pkg assets fonts cursors
 //go:generate g3nicodes -pkg icon icon/codepoints icon/icodes.go
