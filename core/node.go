g3n/engine

commit 8ff00c41085ce75e688b93d2ad87c06440ad4dcd

implemented gltf animation

core/node.go
@@ -573,7 +573,7 @@ func (n *Node) Direction() math32.Vector3 {
 func (n *Node) SetMatrix(m *math32.Matrix4) {
 
 	n.matrix = *m
-	n.changed = true
+	n.matrix.Decompose(&n.position, &n.quaternion, &n.scale)
 }
 
 // Matrix returns a copy of the local transformation matrix.

animation/animation.go
@@ -42,6 +42,8 @@ func (anim *Animation) Name() string {
 func (anim *Animation) Reset() {
 
 	anim.time = anim.start
+	anim.paused = false
+	// TODO reset channels?
 }
 
 // SetPaused sets whether the animation is paused.
@@ -77,25 +79,26 @@ func (anim *Animation) SetStart(v float32) {
 // Update interpolates and updates the target values for each channel.
 // If the animation is paused, returns false. If the animation is not paused,
 // returns true if the input value is inside the key frames ranges or false otherwise.
-func (anim *Animation) Update(delta float32) bool {
+func (anim *Animation) Update(delta float32) {
 
 	// Check if paused
 	if anim.paused {
-		return false
+		return
 	}
 
 	// Check if input is less than minimum
 	anim.time = anim.time + delta
 	if anim.time < anim.minTime {
-		return false
+		return
 	}
 
 	// Check if input is greater than maximum
 	if anim.time > anim.maxTime {
 		if anim.loop {
-			anim.Reset()
+			anim.time = anim.time - anim.maxTime
 		} else {
-			return false
+			anim.time = anim.maxTime - 0.000001
+			anim.SetPaused(true)
 		}
 	}
 
@@ -104,8 +107,6 @@ func (anim *Animation) Update(delta float32) bool {
 		ch := anim.channels[i]
 		ch.Update(anim.time)
 	}
-
-	return true
 }
 
 // AddChannel adds a channel to the animation.
@@ -118,12 +119,12 @@ func (anim *Animation) AddChannel(ch IChannel) {
 
 	// Update maxTime and minTime values
 	kf := ch.Keyframes()
-	firstKf := kf[0]
-	if anim.minTime > firstKf {
-		anim.minTime = firstKf
+	firstTime := kf[0]
+	if anim.minTime > firstTime {
+		anim.minTime = firstTime
 	}
-	lastKf := kf[len(kf)-1]
-	if anim.maxTime < lastKf {
-		anim.maxTime = lastKf
+	lastTime := kf[len(kf)-1]
+	if anim.maxTime < lastTime {
+		anim.maxTime = lastTime
 	}
 }

animation/channel.go
@@ -103,6 +103,7 @@ func (c *Channel) Update(time float32) {
 // IChannel is the interface for all channel types.
 type IChannel interface {
 	Update(time float32)
+	SetBuffers(keyframes, values math32.ArrayF32)
 	Keyframes() math32.ArrayF32
 	Values() math32.ArrayF32
 	SetInterpolationType(it InterpolationType)
@@ -140,6 +141,14 @@ func NewPositionChannel(node core.INode) *PositionChannel {
 				v1.Lerp(&v2, k)
 				node.SetPositionVec(&v1)
 			}
+		case CUBICSPLINE: // TODO
+			pc.interpAction = func(idx int, k float32) {
+				var v1, v2 math32.Vector3
+				pc.values.GetVector3(idx*3, &v1)
+				pc.values.GetVector3((idx+1)*3, &v2)
+				v1.Lerp(&v2, k)
+				node.SetPositionVec(&v1)
+			}
 		}
 	}
 	pc.SetInterpolationType(LINEAR)
@@ -169,9 +178,23 @@ func NewRotationChannel(node core.INode) *RotationChannel {
 				var q1, q2 math32.Vector4
 				rc.values.GetVector4(idx*4, &q1)
 				rc.values.GetVector4((idx+1)*4, &q2)
+				q1.Lerp(&q2, k)
 				quat1 := math32.NewQuaternion(q1.X, q1.Y, q1.Z, q1.W)
-				quat2 := math32.NewQuaternion(q2.X, q2.Y, q2.Z, q2.W)
-				quat1.Slerp(quat2, k)
+				// TODO spherical linear interpolation (slerp) not working as expected for some reason (doing a lerp for now)
+				//quat2 := math32.NewQuaternion(q2.X, q2.Y, q2.Z, q2.W)
+				//quat1.Slerp(quat2, k)
+				node.SetQuaternionQuat(quat1)
+			}
+		case CUBICSPLINE: // TODO
+			rc.interpAction = func(idx int, k float32) {
+				var q1, q2 math32.Vector4
+				rc.values.GetVector4(idx*4, &q1)
+				rc.values.GetVector4((idx+1)*4, &q2)
+				q1.Lerp(&q2, k)
+				quat1 := math32.NewQuaternion(q1.X, q1.Y, q1.Z, q1.W)
+				// TODO spherical linear interpolation (slerp) not working for some reason
+				//quat2 := math32.NewQuaternion(q2.X, q2.Y, q2.Z, q2.W)
+				//quat1.Slerp(quat2, k)
 				node.SetQuaternionQuat(quat1)
 			}
 		}
@@ -206,6 +229,14 @@ func NewScaleChannel(node core.INode) *ScaleChannel {
 				v1.Lerp(&v2, k)
 				node.SetScaleVec(&v1)
 			}
+		case CUBICSPLINE: // TODO
+			sc.interpAction = func(idx int, k float32) {
+				var v1, v2 math32.Vector3
+				sc.values.GetVector3(idx*3, &v1)
+				sc.values.GetVector3((idx+1)*3, &v2)
+				v1.Lerp(&v2, k)
+				node.SetScaleVec(&v1)
+			}
 		}
 	}
 	sc.SetInterpolationType(LINEAR)
@@ -213,11 +244,11 @@ func NewScaleChannel(node core.INode) *ScaleChannel {
 }
 
 // InterpolationType specifies the interpolation type.
-type InterpolationType int
+type InterpolationType string
 
 // The various interpolation types.
 const (
-	STEP        = InterpolationType(iota) // The animated values remain constant to the output of the first keyframe, until the next keyframe.
-	LINEAR                                // The animated values are linearly interpolated between keyframes. Spherical linear interpolation (slerp) is used to interpolate quaternions.
-	CUBICSPLINE                           // TODO
+	STEP        = InterpolationType("STEP")          // The animated values remain constant to the output of the first keyframe, until the next keyframe.
+	LINEAR      = InterpolationType("LINEAR")        // The animated values are linearly interpolated between keyframes. Spherical linear interpolation (slerp) is used to interpolate quaternions.
+	CUBICSPLINE = InterpolationType("CUBICSPLINE")   // TODO
 )

loader/gltf/gltf.go
@@ -5,6 +5,8 @@
 // Package gltf
 package gltf
 
+import "github.com/g3n/engine/core"
+
 // GLTF is the root object for a glTF asset.
 type GLTF struct {
 	ExtensionsUsed     []string               // Names of glTF extensions used somewhere in this asset. Not required.
@@ -143,16 +145,16 @@ type Indices struct {
 // Material describes the material appearance of a primitive.
 type Material struct {
 	Name                 string                 // The user-defined name of this object. Not required.
-	Extensions           map[string]interface{} // Dictionary object with extension-specific objects. Not required.
-	Extras               interface{}            // Application-specific data. Not required.
 	PbrMetallicRoughness *PbrMetallicRoughness  // A set of parameter values that are used to define the metallic-roughness material model from Physically-Based Rendering (PBR) methodology. When not specified, all the default values of pbrMetallicRoughness apply. Not required.
 	NormalTexture        *NormalTextureInfo     // The normal map texture. Not required.
 	OcclusionTexture     *OcclusionTextureInfo  // The occlusion map texture. Not required.
 	EmissiveTexture      *TextureInfo           // The emissive map texture. Not required.
-	EmissiveFactor       [3]float32             // The emissive color of the material. Not required. Default is [0,0,0]
+	EmissiveFactor       *[3]float32            // The emissive color of the material. Not required. Default is [0,0,0]
 	AlphaMode            string                 // The alpha rendering mode of the material. Not required. Default is OPAQUE.
 	AlphaCutoff          float32                // The alpha cutoff value of the material. Not required. Default is 0.5.
 	DoubleSided          bool                   // Specifies whether the material is double sided. Not required. Default is false.
+	Extensions           map[string]interface{} // Dictionary object with extension-specific objects. Not required.
+	Extras               interface{}            // Application-specific data. Not required.
 }
 
 // Mesh is a set of primitives to be rendered.
@@ -184,6 +186,8 @@ type Node struct {
 	Name        string                 // The user-defined name of this object. Not required.
 	Extensions  map[string]interface{} // Dictionary object with extension-specific objects. Not required.
 	Extras      interface{}            // Application-specific data. Not required.
+
+	node        core.INode             // Cached node
 }
 
 // TODO Why not combine NormalTextureInfo and OcclusionTextureInfo ? Or simply add Scale to TextureInfo and use only TextureInfo?
@@ -300,7 +304,7 @@ type Target struct {
 
 // Texture represents a texture and its sampler.
 type Texture struct {
-	Sampler    int                    // The index of the sampler used by this texture. When undefined, a sampler with REPEAT wrapping and AUTO filtering should be used. Not required.
+	Sampler    *int                   // The index of the sampler used by this texture. When undefined, a sampler with REPEAT wrapping and AUTO filtering should be used. Not required.
 	Source     int                    // The index of the image used by this texture. Not required.
 	Name       string                 // The user-defined name of this object. Not required.
 	Extensions map[string]interface{} // Dictionary object with extension-specific objects. Not required. Not required.

loader/gltf/material_pbr.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/g3n/engine/material"
 	"github.com/g3n/engine/math32"
-	"github.com/g3n/engine/texture"
 )
 
 func (g *GLTF) loadMaterialPBR(m *Material) (material.IMaterial, error) {
@@ -18,8 +17,6 @@ func (g *GLTF) loadMaterialPBR(m *Material) (material.IMaterial, error) {
 
 	// Create new physically based material
 	pm := material.NewPhysical()
-
-	// TODO emmisive factor, emmissive map, occlusion, etc...
 
 	// BaseColorFactor
 	var baseColorFactor math32.Color4
@@ -48,15 +45,62 @@ func (g *GLTF) loadMaterialPBR(m *Material) (material.IMaterial, error) {
 	}
 	pm.SetRoughnessFactor(roughnessFactor)
 
+	// EmissiveFactor
+	var emissiveFactor math32.Color
+	if m.EmissiveFactor != nil {
+		emissiveFactor = math32.Color{m.EmissiveFactor[0], m.EmissiveFactor[1], m.EmissiveFactor[2]}
+	} else {
+		if m.EmissiveTexture != nil {
+			emissiveFactor = math32.Color{1, 1, 1}
+		} else {
+			emissiveFactor = math32.Color{0,0,0}
+		}
+	}
+	pm.SetEmissiveFactor(&emissiveFactor)
+
 	// BaseColorTexture
-	var tex *texture.Texture2D
-	var err error
 	if pbr.BaseColorTexture != nil {
-		tex, err = g.loadTextureInfo(pbr.BaseColorTexture)
+		tex, err := g.loadTextureInfo(pbr.BaseColorTexture)
 		if err != nil {
 			return nil, err
 		}
 		pm.SetBaseColorMap(tex)
+	}
+
+	// MetallicRoughnessTexture
+	if pbr.MetallicRoughnessTexture != nil {
+		tex, err := g.loadTextureInfo(pbr.MetallicRoughnessTexture)
+		if err != nil {
+			return nil, err
+		}
+		pm.SetMetallicRoughnessMap(tex)
+	}
+
+	// NormalTexture
+	if m.NormalTexture != nil {
+		tex, err := g.loadTexture(m.NormalTexture.Index)
+		if err != nil {
+			return nil, err
+		}
+		pm.SetNormalMap(tex)
+	}
+
+	// OcclusionTexture
+	if m.OcclusionTexture != nil {
+		tex, err := g.loadTexture(m.OcclusionTexture.Index)
+		if err != nil {
+			return nil, err
+		}
+		pm.SetOcclusionMap(tex)
+	}
+
+	// EmissiveTexture
+	if m.EmissiveTexture != nil {
+		tex, err := g.loadTexture(m.EmissiveTexture.Index)
+		if err != nil {
+			return nil, err
+		}
+		pm.SetEmissiveMap(tex)
 	}
 
 	return pm, nil

commit 685bcdbb1ac308642295dfcc2180dec54618b4ae

Fix raycasts: the least obvious way
Apparently, commit 8ff00c41 broke raycasts. I don't think I understand
why exactly that happened, but setting 'changed' to true after changing
node's matrix seems legit.

core/node.go
@@ -632,6 +632,7 @@ func (n *Node) SetMatrix(m *math32.Matrix4) {
 
 	n.matrix = *m
 	n.matrix.Decompose(&n.position, &n.quaternion, &n.scale)
+	n.changed = true
 }
 
 // Matrix returns a copy of the local transformation matrix.