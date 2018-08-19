4d1094e9cc2772a67f88900b26c03f0cf63f01e9
Daniel Salvadori <danaugrs@gmail.com>
2018-08-19 12:01:22 -0300

    fixed Vector3.AlmostEquals()

--- math32/vector3.go
+++ math32/vector3.go
@@ -671,9 +671,9 @@ func (v *Vector3) RandomTangents() (*Vector3, *Vector3) {
 // AlmostEquals returns whether the vector is almost equal to another vector within the specified tolerance.
 func (v *Vector3) AlmostEquals(other *Vector3, tolerance float32) bool {
 
-	if (v.X - other.X < tolerance) &&
-		(v.Y - other.Y < tolerance) &&
-		(v.Z - other.Z < tolerance) {
+	if (Abs(v.X - other.X) < tolerance) &&
+		(Abs(v.Y - other.Y) < tolerance) &&
+		(Abs(v.Z - other.Z) < tolerance) {
 			return true
 	}
 	return false
