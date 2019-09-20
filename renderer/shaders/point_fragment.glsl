b55398cb56314151523038ae7bbf32c8fd35bee6
Daniel Salvadori <danaugrs@gmail.com>
2019-09-20 18:00:15 -0300

    Fix constant texture indexing for points

--- renderer/shaders/point_fragment.glsl
+++ renderer/shaders/point_fragment.glsl
@@ -2,24 +2,6 @@ precision highp float;
 
 #include <material>
 
-// GLSL 3.30 does not allow indexing texture sampler with non constant values.
-// This macro is used to mix the texture with the specified index with the material color.
-// It should be called for each texture index.
-#if MAT_TEXTURES > 0
-vec4 MIX_POINT_TEXTURE(vec4 texMixed, mat2 rotation, int i) {                                                           \
-    if (MatTexVisible(i)) {                                                                                      \
-        vec2 pt = gl_PointCoord - vec2(0.5);                                                                     \
-        vec4 texColor = texture(MatTexture[i], (rotation * pt + vec2(0.5)) * MatTexRepeat(i) + MatTexOffset(i)); \
-        if (i == 0) {                                                                                            \
-            texMixed = texColor;                                                                                 \
-        } else {                                                                                                 \
-            texMixed = mix(texMixed, texColor, texColor.a);                                                      \
-        }                                                                                                        \
-    }
-    return texMixed;
-}
-#endif
-
 // Inputs from vertex shader
 in vec3 Color;
 flat in mat2 Rotation;
@@ -29,17 +11,42 @@ out vec4 FragColor;
 
 void main() {
 
-    // Mix material color with textures colors
+    // Compute final texture color
     vec4 texMixed = vec4(1);
-    #if MAT_TEXTURES==1
-        texMixed = MIX_POINT_TEXTURE(texMixed, Rotation, 0);
-    #elif MAT_TEXTURES==2
-        texMixed = MIX_POINT_TEXTURE(texMixed, Rotation, 0);
-        texMixed = MIX_POINT_TEXTURE(texMixed, Rotation, 1);
-    #elif MAT_TEXTURES==3
-        texMixed = MIX_POINT_TEXTURE(texMixed, Rotation, 0);
-        texMixed = MIX_POINT_TEXTURE(texMixed, Rotation, 1);
-        texMixed = MIX_POINT_TEXTURE(texMixed, Rotation, 2);
+    #if MAT_TEXTURES > 0
+        vec2 pointCoord = Rotation * gl_PointCoord - vec2(0.5) + vec2(0.5);
+        bool firstTex = true;
+        if (MatTexVisible(0)) {
+            vec4 texColor = texture(MatTexture[0], pointCoord * MatTexRepeat(0) + MatTexOffset(0));
+            if (firstTex) {
+                texMixed = texColor;
+                firstTex = false;
+            } else {
+                texMixed = Blend(texMixed, texColor);
+            }
+        }
+        #if MAT_TEXTURES > 1
+            if (MatTexVisible(1)) {
+                vec4 texColor = texture(MatTexture[1], pointCoord * MatTexRepeat(1) + MatTexOffset(1));
+                if (firstTex) {
+                    texMixed = texColor;
+                    firstTex = false;
+                } else {
+                    texMixed = Blend(texMixed, texColor);
+                }
+            }
+            #if MAT_TEXTURES > 2
+                if (MatTexVisible(2)) {
+                    vec4 texColor = texture(MatTexture[2], pointCoord * MatTexRepeat(2) + MatTexOffset(2));
+                    if (firstTex) {
+                        texMixed = texColor;
+                        firstTex = false;
+                    } else {
+                        texMixed = Blend(texMixed, texColor);
+                    }
+                }
+            #endif
+        #endif
     #endif
 
     // Generates final color
