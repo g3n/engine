dd3ce190cd299cc51640c3d9c4d079faf79aa4eb
Daniel Salvadori <danaugrs@gmail.com>
2019-09-22 11:10:51 -0300

    Consolidate logger docs

--- util/logger/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2016 The G3N Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package logger implements an hierarchical logger used by other packages.
-package logger
--- util/logger/logger.go
+++ util/logger/logger.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package logger implements an hierarchical logger used by other packages.
 package logger
 
 import (
