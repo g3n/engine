wolfgarnet/g3n-engine

Added get ith element of vector

math32/vector2.go
@@ -404,3 +404,15 @@ func (v *Vector2) InTriangle(p0, p1, p2 *Vector2) bool {
 
 	return s >= 0 && t >= 0 && (s+t) < 2*A*sign
 }
+
+// GetElement gets the ith element of the vector.
+func (v *Vector2) GetElement(i int) float32 {
+	switch i {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	default:
+		panic("No such index")
+	}
+}

math32/vector3.go
@@ -678,3 +678,17 @@ func (v *Vector3) AlmostEquals(other *Vector3, tolerance float32) bool {
 	}
 	return false
 }
+
+// GetElement gets the ith element of the vector.
+func (v *Vector3) GetElement(i int) float32 {
+	switch i {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	case 2:
+		return v.Z
+	default:
+		panic("No such index")
+	}
+}

math32/vector4.go
@@ -630,3 +630,19 @@ func (v *Vector4) Clone() *Vector4 {
 
 	return NewVector4(v.X, v.Y, v.Z, v.W)
 }
+
+// GetElement gets the ith element of the vector.
+func (v *Vector4) GetElement(i int) float32 {
+	switch i {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	case 2:
+		return v.Z
+	case 3:
+		return v.W
+	default:
+		panic("No such index")
+	}
+}

g3n/engine

math32/vector2.go
@@ -404,3 +404,15 @@ func (v *Vector2) InTriangle(p0, p1, p2 *Vector2) bool {
 
 	return s >= 0 && t >= 0 && (s+t) < 2*A*sign
 }
+
+// GetAxis gets the ith axis of the vector.
+func (v *Vector2) GetAxis(i int) float32 {
+	switch i {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	default:
+		panic("No such index")
+	}
+}

math32/vector3.go
@@ -678,3 +678,17 @@ func (v *Vector3) AlmostEquals(other *Vector3, tolerance float32) bool {
 	}
 	return false
 }
+
+// GetAxis gets the ith axis of the vector.
+func (v *Vector3) GetAxis(i int) float32 {
+	switch i {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	case 2:
+		return v.Z
+	default:
+		panic("No such index")
+	}
+}

math32/vector4.go
@@ -630,3 +630,19 @@ func (v *Vector4) Clone() *Vector4 {
 
 	return NewVector4(v.X, v.Y, v.Z, v.W)
 }
+
+// GetAxis gets the ith axis of the vector.
+func (v *Vector4) GetAxis(i int) float32 {
+	switch i {
+	case 0:
+		return v.X
+	case 1:
+		return v.Y
+	case 2:
+		return v.Z
+	case 3:
+		return v.W
+	default:
+		panic("No such index")
+	}
+}